4b4a51f911bcc67e3a972cce152d4377466535e4
Colin Barringer <colin@barringer.dev>
2021-04-02 17:13:36 -0700

    display stats in a table

--- front-end/src/components/PlayerStats.tsx
+++ front-end/src/components/PlayerStats.tsx
@@ -1,8 +1,23 @@
-import { selectorFamily, useRecoilValue } from 'recoil';
+import React from 'react';
 import ReactJson from 'react-json-view';
+import { selectorFamily, useRecoilValue } from 'recoil';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';  
 
 import { getPlayerStats } from '../api';
 import { Player } from '../types';
+import { deNamespace } from '../utils';
 
 const playerStatsQuery = selectorFamily<any,string>({
   key: 'PlayerStats',
@@ -15,5 +30,43 @@ interface PlayerStatsProps {
 
 export default function PlayerStats({ player }: PlayerStatsProps) {
   const data = useRecoilValue(playerStatsQuery(player.uuid));
-  return <ReactJson src={data} />;
+  const categories = Object.keys(data.stats).sort();
+  const [category, setCategory] = React.useState(categories[0]);
+  const handleChange = React.useCallback((event: React.ChangeEvent<{ value: unknown}>) => {
+    setCategory(event.target.value as string);
+  }, [categories, category]);
+  return (
+    <>
+      <FormControl style={{ minWidth: 120 }}>
+        <InputLabel>Category</InputLabel>
+        <Select
+          value={category}
+          onChange={handleChange}
+        >
+          {categories.map(id => <MenuItem key={id} value={id}>{deNamespace(id)}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <br /><br />
+      <TableContainer component={Paper}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell><>Stat</></TableCell>
+              <TableCell><>Count</></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.keys(data.stats[category]).sort((a, b) => {
+              return data.stats[category][b] - data.stats[category][a];
+            }).map(id => (
+              <TableRow key={id}>
+                <TableCell><>{deNamespace(id)}</></TableCell>
+                <TableCell><>{data.stats[category][id]}</></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
