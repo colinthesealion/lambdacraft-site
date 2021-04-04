62684c9afb1e25b4143196d52d8ecd2601fcdbb9
Colin Barringer <colin@barringer.dev>
2021-04-04 16:20:50 -0700

    fixed layout for MapGrid component

--- front-end/src/components/MapGrid.tsx
+++ front-end/src/components/MapGrid.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { GridList } from '@material-ui/core';
+
 import { getMapURL } from '../api';
 
 interface MapGridProps {
@@ -6,9 +9,9 @@ interface MapGridProps {
 
 export default function MapGrid({ rows }: MapGridProps) {
   return (
-    <>
+    <GridList cols={rows[0].length} cellHeight={128} style={{ width: 128 * rows[0].length }}>
       {rows.map((row, index) => (
-        <div key={index}>
+        <React.Fragment key={index}>
           {row.map(id => (
             <img
               key={id}
@@ -17,8 +20,9 @@ export default function MapGrid({ rows }: MapGridProps) {
               src={getMapURL(id)}
             />
           ))}
-        </div>
+          <br />
+        </React.Fragment>
       ))}
-    </>
+    </GridList>
   );
 }
